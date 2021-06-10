745fa63e99f36fb00eefbba6ee1ad512c72bf420
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-10 20:51:10 +0530

    Added Use case 1 to check if employee is present

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,9 +1,18 @@
-public class EmpWageBuilder {
+public class EmpWageBuilderUC1 {
 
         public static void main(String[] args) {
 
-        System.out.println(" Welcome to Employee wage computation program ");
+	System.out.println("Welcome to Employee wage computation program ");
 
+        // Constants
+        int IS_FULL_TIME = 1;
+
+        // Computation
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empCheck == IS_FULL_TIME)
+                System.out.println("Employee is present");
+        else
+                System.out.println("Employee is absent");
         }
 }
-
