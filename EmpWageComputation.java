7303c26df634dc8d7a48086ea9bc4ab97ccc8985
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-10 20:58:35 +0530

    Added use case 3 to check whether full time or part time

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,7 +5,8 @@ public class EmpWageBuilderUC1 {
 	System.out.println("Welcome to Employee wage computation program ");
 
         // Constants
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
 
         // Variables
@@ -13,12 +14,12 @@ public class EmpWageBuilderUC1 {
         int empWage = 0;
 
         // Computations
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
 
         if (empCheck == IS_FULL_TIME)
                 empHrs = 8;
-        else
-                empHrs = 0;
+        else if (empCheck == IS_PART_TIME)
+                empHrs = 4;
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Employee Wage : " + empWage);
         }
