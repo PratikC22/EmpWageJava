4ef7088ae63e8d5315098ce72eea4ac37f4d148a
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-15 23:21:00 +0530

    added use case 8

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,42 +1,66 @@
 /**
- * Employee wage computation problem.
- *
+ * Employee wage program using oops.
+ * 
  * @author Pratik Chaudhari
  * @since 14/06/2021
  */
-package EmpWage;
+package empWage;
 
 public class Wage {
-    // Constants.
-    public static final int FULL_TIME = 1;
-    public static final int PART_TIME = 2;
-    public static final int WAGE_PER_HOUR = 20;
-    public static final int DAYS_PER_MONTH = 20;
-    public static final int MAX_WORK_HOURS = 100;
+	// Instance variables
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 
-    // Printing welcome statement.
-    void greet() {      // Printing welcome statement.
-        System.out.println("Welcome to employee wage computation.");
-    }
-    // Calculating employee wage
-    void empWage() {
-        int empHours,totalHours = 0, workingDays = 0;           // Variables
-        while (workingDays <= DAYS_PER_MONTH && totalHours <= MAX_WORK_HOURS) {
-            int empCheck = (int) (Math.floor(Math.random() * 10)) % 3;          //Using random for generating employee attendance.
-            empHours = switch (empCheck) {          //Using switch to simulate full time, part time or absent.
-                case FULL_TIME -> 8;        // If full time then empHrs = 8.
-                case PART_TIME -> 4;        // If part time then empHrs = 4.
-                default -> 0;               // if absent then empHrs = 0.
-            };
-            totalHours = totalHours + empHours;     // Calculating total working hours.
-            workingDays++;
-        }
-        int dailyWage = totalHours * WAGE_PER_HOUR;     // Calculating daily wage.
-        System.out.println("Employee wage is "+dailyWage);
-    }
-    public static void main(String[] args) {
-        Wage Employee = new Wage();     // Creating object named Employee.
-        Employee.greet();           // Calling greet method.
-        Employee.empWage();         // Calling empWage method.
-    }
+	private final String company;
+	private final int wagePerHour;
+	private final int workingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+
+	// Constructor
+	public Wage(String company, int wagePerHour, int workingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.wagePerHour = wagePerHour;
+		this.workingDays = workingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	// Calculating working hours and total wage
+	public void computeWage() {
+		// Variables
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		// Computation
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < workingDays) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			// System.out.println("Day : " + totalWorkingDays + " Emp Hr : " + empHrs);
+		}
+		totalEmpWage = totalEmpHrs * wagePerHour;
+	}
+
+	@Override
+	public String toString() {
+		return "Total Emp Wage for company " + company + " is " + totalEmpWage;
+	}
+
+	public static void main(String[] args) {
+
+		Wage dMart = new Wage("DMart", 20, 20, 100);
+		Wage reliance = new Wage("Reliance", 10, 40, 200);
+		dMart.computeWage(); // Compute for dMart
+		System.out.println(dMart);
+		reliance.computeWage(); // Compute for reliance
+		System.out.println(reliance);
+	}
 }
